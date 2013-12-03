65505b9e9260c27051f8edd67b04bf872c4e5012
Satoru Takanami <satorutakanami@gmail.com>
2013-12-03 19:42:06 +0900

    箇条書き番号が不完全だったため修正

--- markdown2IndexList.js
+++ markdown2IndexList.js
@@ -12,22 +12,22 @@ function makeIndent (nest) {
 function addIndex (no, nest, sectionStr) {
 	var sectionRegex = new RegExp('\n' + '\t{' + nest + '}[^\t]. ');
 	var section = sectionStr.split(sectionRegex);
+	if (section[0]) section[0] = section[0].replace(/[\t\d.]+ /, '');
 	if (section[0] === '') section.shift();
-	section[0] = section[0].replace(/[\t\d.]+ /, '');
 	for( var i = 0; i < section.length; i++ ) {
 		var headlines = section[i].split('\n');
 		var indent = makeIndent(nest);
-		console.log(indent + no + (i + 1) + ' ' + headlines[0]);
-		output += indent + no + (i + 1) + ' ' + headlines[0] + '\n'
-		if (headlines.length > 1) {
-			addIndex ((i+1)+'.', nest+1, headlines.slice(1).join('\n') );
-		}
+		var sectionNo = no.concat();
+		sectionNo.push(String(i+1));
+		console.log(indent + sectionNo.join('.') + ' ' + headlines[0]);
+		output += indent + sectionNo.join('.') + ' ' + headlines[0] + '\n'
+		if (headlines.length > 1) addIndex(sectionNo, nest+1, headlines.slice(1).join('\n') );
 	}
 }
 
 var convert = function (error, data) {
 	var dataStr = data.toString().replace(/#.+#\n/, '');
-	addIndex('', 0, '\n' + dataStr);
+	addIndex([], 0, '\n' + dataStr);
 	fs.writeFile('index.txt', output);
 };
 
