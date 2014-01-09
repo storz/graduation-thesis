7970ab0ab56cafd0bb4745438e94a1f35e4fcaad
storz <distorz@gmail.com>
2014-01-09 23:12:20 +0900

    5章結果以外少しだけ

--- src/patternGenerator.js
+++ src/patternGenerator.js
@@ -1,4 +1,13 @@
 var patterns = [0,1,2,3];
+var i = 0;
+
+
+function c(n) {
+	if (n === 0) return 'A';
+	if (n === 1) return 'B';
+	if (n === 2) return 'C';
+	if (n === 3) return 'D';
+}
 
 patterns.forEach(function (first, index, patterns) {
 	var newPat = patterns.concat();
@@ -10,7 +19,8 @@ patterns.forEach(function (first, index, patterns) {
 			var newPat = patterns.concat()
 			newPat.splice(index, 1)
 			newPat.forEach(function (fourth, index, patterns) {
-				console.log('[' + first + ',' + second + ',' + third + ',' + fourth + '],');
+				console.log(i + ' & ' + c(first) + '→' + c(second) + '→' + c(third) + '→' + c(fourth) + ' \\\\');
+				i++;
 			})
 		})
 	})
